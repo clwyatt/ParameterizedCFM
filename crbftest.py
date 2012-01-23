1d9c3575d271e8cec0586c23210e2683bf6a1c12
Chris Wyatt <clwyatt@vt.edu>
2012-01-23 11:47:34 -0500

    snapshot before adding derivatives

---
+++ crbftest.py
@@ -0,0 +1,26 @@
+import math
+import numpy as num
+import matplotlib.pyplot as plt
+
+def csrbf(r):
+    """compactly supported radial basis function of degree one"""
+    def f(rp):
+        return num.power((num.maximum(0, 1-rp)), 2)*(2*rp+1)
+    fvec = num.vectorize(f)
+    return fvec(r)
+
+def dcsrbf(r):
+    """derivative compactly supported radial basis function of degree one"""
+    def f(rp):
+        return -6*rp*(num.maximum(0, 1-rp))
+    fvec = num.vectorize(f)
+    return fvec(r)
+
+def test():
+    (r, step) = num.linspace(0, 2, 1000, retstep=True)
+
+    plt.plot(r, csrbf(r))
+    plt.plot(r, dcsrbf(r))
+    plt.xlabel('r')
+    plt.show()
+
--- test1d.py
+++ test1d.py
@@ -1,37 +1,52 @@
 ### @export "imports"
-import numpy as num
-import matplotlib
-import matplotlib.pyplot as plt
 import math
+import numpy as num
+import matplotlib.pyplot as plt
+from matplotlib.mlab import normpdf 
 from scipy.integrate import trapz
+from scipy.optimize import fmin_bfgs
 
 ### @export "kernel"
 def kernel(sigma):
     sz = 6*num.ceil(sigma)+1
     x = num.linspace(-3*sigma, 3*sigma, sz);
-    return matplotlib.mlab.normpdf(x, 0, sigma)
-    
+    return normpdf(x, 0, sigma)
+
+### @export "phantom1"
 def makeI1():
     phantom = num.zeros(1000)
     phantom[250:750] = 1;
     return num.convolve(phantom, kernel(10), 'same')
 
+### @export "phantom2"
+def makeI2():
+    phantom = num.zeros(1000)
+    phantom[250:750] = 1
+    phantom[490:510] = 1.2
+    return num.convolve(phantom, kernel(10), 'same')
+
+### @export "phantom0"
 def makeI0(offset):
     phantom = num.zeros(1000)
     phantom[(250+offset):(750+offset)] = 1;
     return num.convolve(phantom, kernel(10), 'same')
 
-def csrbf(n, l, r):
+### @export "csrbf"
+def csrbf(r):
     """compactly supported radial basis function of degree one"""
-    p = num.floor(n/2) + l + 1
-    return num.power((num.maximum(0, 1-r)), p+1)*((p+1)*r+1)
+    return num.power((num.maximum(0, 1-rp)), 2)*(2*rp+1)
 
+### @export "dcsrbf"
+def dcsrbf(r):
+    """derivative compactly supported radial basis function of degree one"""
+    return -6*rp*(num.maximum(0, 1-rp))
+
+### @export "levelset"
 def phi(x, alpha, beta, gamma):
     """paramterized levelset function"""
     def f(xp, a, b, g):
         delta = xp - g
-        return a*csrbf(1,1,abs(b*delta))
-
+        return a*csrbf(math.sqrt(b*delta*b*delta + 0.001))
     vphi = num.vectorize(f);
     n = num.size(alpha)
     sum = num.zeros(num.size(x));
@@ -39,53 +54,112 @@ def phi(x, alpha, beta, gamma):
         sum += vphi(x, alpha[j], beta[j], gamma[j])
     return sum
 
-def heaviside(x, eps):
-    """A continous approximation to the Heaviside function"""
+def dphidalpha_j(x, alpha_j, beta_j, gamma_j):
+    """derivative of phi wrt alpha_j"""
+    delta = x - gamma_j
+    return csrbf(math.sqrt(beta_j*delta*beta_j*delta))
+
+def dphidbeta_j(x, alpha_j, beta_j, gamma_j):
+    delta = x - gamma_j
+    r = math.sqrt(beta_j*delta*beta_j*delta) + 0.001
+    return alpha_j*dcsrbf(r)*(beta_j*delta*delta)/r
+
+def dphidgamma_j(x, alpha_j, beta_j, gamma_j):
+    delta = x - gamma_j
+    r = math.sqrt(beta_j*delta*beta_j*delta) + 0.001
+    return alpha_j*dcsrbf(r)*(-beta_j*beta_j*delta)/r
+
+### @export "H"
+def heaviside(x, eps=0.001):
+    """A continuous approximation to the Heaviside function"""
     def f(xp):
         return 0.5*(1 + (2/math.pi)*math.atan(math.pi*xp/eps))
-
     vh = num.vectorize(f)
     return vh(x)
 
+#def heaviside(x, eps=0.001):
+#    """A continuous approximation to the Heaviside function"""
+#    def f(xp):
+#        if(xp > eps): return 1
+#        if(xp < eps): return 0
+#        return 0.5*(1 + xp/eps + (1/math.pi)*math.sin(math.pi*xp/eps))
+#    vh = num.vectorize(f)
+#    return vh(x)
+
+### @export "objective"
 def f(I0, I1, tau, alpha, beta, gamma, lam, mu):
     """The objective function"""
     (x, step) = num.linspace(-1, 1, 1000, retstep=True)
     def integrand1(xp):
-        return tau*tau*heaviside(phi(xp,alpha, beta,gamma), 0.01)
-
+        return tau*tau*heaviside(-phi(xp,alpha, beta,gamma))
     def integrand2(xp):
         moving = num.interp(xp-tau, xp, I0)
-        residual = I1 - I0;
-        return heaviside(phi(xp,alpha, beta,gamma),0.01)*num.power(residual, 2)
-        
+        residual = I1 - moving;
+        return heaviside(-phi(xp,alpha, beta,gamma))*num.power(residual, 2)
     def integrand3(xp):
-        return heaviside(phi(xp,alpha, beta,gamma), 0.01)
-        
-    return trapz(integrand1(x)+lam*integrand2(x)+mu*integrand3(x), dx=step)
+        return heaviside(phi(xp,alpha, beta,gamma))
+    return trapz(lam*integrand2(x)+mu*integrand3(x), dx=step)
 
-                                 
-#    term1 = num.add.reduce(tau*tau*vh(phi(x, alpha, beta, gamma),0.01))
-#    moving = interpolate(x-tau,I0)
-#    residual = num.power(I1 - moving,2)
-#    term2 = num.add.reduce(vh(phi(x, alpha, beta, gamma),0.01))
-    
+### @export "wrapper"
+def wrapper(theta, I0, I1, lam, mu):
+    return f(I0, I1, theta[0], theta[1:8], theta[8:15], theta[15:23], lam, mu)
+
+def monitor(x):
+    """Callback during optimization"""
+    print x
+
+### @export "test"
 def test():
-    x = num.linspace(-1, 1, 1000)
+    (x, step) = num.linspace(-1, 1, 1000, retstep=True)
 
-    I1 = makeI1()
-    I0 = makeI0(10)
+    T = makeI2()
+    M = makeI0(100)
 
-    a = num.zeros(1)
-    B = 4*num.ones(1)
-    g = -0.5*num.ones(1)
+    lam = 10
+    mu = 0.01
+
+    m0 = 7
+    theta0 = num.zeros(1+m0*3)
+    theta0[0] = -step*100 #initial translation
+    theta0[1] = -0.2 #initial alpha_1
+    theta0[2] = 0.2 #initial alpha_2
+    theta0[3] = -0.2 #initial alpha_3
+    theta0[4] = 0.2 #initial alpha_4
+    theta0[5] = -0.2 #initial alpha_5
+    theta0[6] = 0.2 #initial alpha_6
+    theta0[7] = -0.2 #initial alpha_7
+                
+    theta0[8:15] = 2.0 #initial beta_j
+
+    theta0[15:23] = num.linspace(-1,1, m0) #initial gamma_j
+
+#    opt = theta0
+    opt = fmin_bfgs(wrapper,
+                    theta0, args=(M, T, lam, mu),
+                    callback=monitor,
+                    maxiter=20)
+
+    print "start"
+    print "     params = ", theta0
+    print "     objective = ", wrapper(theta0, M, T, lam, mu)
+    print "     ROI size = ", trapz(heaviside(phi(x,theta0[1:8], theta0[8:15], theta0[15:23])), dx=step)
+    print "end"
+    print "   params = ", opt
+    print "   objective = ", wrapper(opt, M, T, lam, mu)
+    print "   ROI size = ", trapz(heaviside(phi(x,opt[1:8], opt[8:15], opt[15:23])), dx=step)
     
-    tau = 0
-    lam = 1
-    mu = 1
+    plt.figure(1)
+    plt.plot(x, heaviside(phi(x, theta0[1:8], theta0[8:15], theta0[15:23])))
+    plt.xlabel('x')
 
-    print f(I0, I1, tau, a, B, g, lam, mu)
+    plt.figure(2)
+    plt.plot(x, heaviside(phi(x, opt[1:8], opt[8:15], opt[15:23])))
+    plt.xlabel('x')
 
-    plt.plot(x, phi(x, a, B, g))
+
+
     plt.show()
+
+
     
     
